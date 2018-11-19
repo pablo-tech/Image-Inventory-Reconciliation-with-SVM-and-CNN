cdffd4535d8fb7d155d92e95d331b21ff55c0adb
pablo <pablo.bertorello@gmail.com>
2018-11-18 20:02:13 -0800

    code from Sravan

--- svm.py
+++ svm.py
@@ -1,4 +1,24 @@
 from sklearn import svm
+import random
+import json
+
+
+random.seed(229)
+
+local_path = ''
+sage_path = '/home/ec2-user/SageMaker/efs/amazon-bin/'
+env_path = local_path
+
+train_file = env_path+'counting_train.json'
+# train_file = env_path+'/input/counting_train.json'
+
+image_path = env_path+'data/Images/'
+# img_path = env_path+'/data/bin-images-resize/'
+
+
+with open(train_file) as f:
+    data_list = json.loads(f.read())
+print("#files_train=", len(data_list))
 
 
 X = [[0, 0], [1, 1]]
