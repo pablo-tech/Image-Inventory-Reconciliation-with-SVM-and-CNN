6492a0e4b547110bf8ec5798ceaf2d83bb529127
pablo <pablo.bertorello@gmail.com>
2018-11-23 18:38:39 -0800

    mean/variance normalization

--- svm-raw.py
+++ svm-raw.py
@@ -45,9 +45,11 @@ def getResizedReshapedMatrix(image):
 def getZeroMeanNormalizedVarianceMatrix(Xmatrix):
     # Mean subtraction: subtracting the mean across every individual feature in the data
     Xmatrix -= np.mean(Xmatrix, axis = 0)
+    print("XmatrixMean=",Xmatrix)
     # Normalization, two wasy:
     # 1) is to divide each dimension by its standard deviation, once it has been zero-centered
     Xmatrix /= np.std(Xmatrix, axis = 0)
+    print("XmatrixVariance=", Xmatrix)
     # 2) Another form of this preprocessing normalizes each dimension so that the min and max along the dimension is -1 and 1 respectively
     return Xmatrix
 
@@ -72,8 +74,7 @@ def getXY(setName):
             try:
                 this_image = imread(env_images_path+file_name)
                 image_resized_reshaped = getResizedReshapedMatrix(this_image)
-                image_mean_variance_normalized = getZeroMeanNormalizedVarianceMatrix(image_resized_reshaped)
-                image_to_use = image_mean_variance_normalized
+                image_to_use = image_resized_reshaped
                 if len(X_set)==0:
                     X_set = image_to_use
                     Y_out = [expected_quantity]
@@ -90,6 +91,11 @@ def getXY(setName):
 X_train,Y_train=getXY("counting_train")
 X_validation,Y_validation=getXY("counting_val")
 
+X_train_mean_variance_normalized = getZeroMeanNormalizedVarianceMatrix(X_train)
+X_validation_mean_variance_normalized = getZeroMeanNormalizedVarianceMatrix(X_validation)
+
+print ("X_train=", X_train_mean_variance_normalized)
+# exit(1)
 
 # PLOT
 # sample_name = "00001"
@@ -112,7 +118,7 @@ print("WILL NOW TRAIN SVM... set_size=", len(Y_train))
 # clf = svm.SVC(gamma='scale')
 clf = svm.NuSVC(nu=0.1) # gamma='scale'
 # clf = svm.LinearSVC(penalty='l2', multi_class='ovr')
-clf.fit(X_train, Y_train)
+clf.fit(X_train_mean_variance_normalized, Y_train)
 
 
 # ACCURACY
@@ -143,7 +149,7 @@ class_success_count = np.zeros(classes_under_study)
 class_count = np.zeros(classes_under_study)
 
 for class_id in range(classes_under_study):
-    class_id_accuracy, class_id_success_count, class_id_count = getAccuracy(X_validation, Y_validation, class_id)
+    class_id_accuracy, class_id_success_count, class_id_count = getAccuracy(X_validation_mean_variance_normalized, Y_validation, class_id)
     print("class_id=", class_id, " class_id_accuracy=",class_id_accuracy, " class_id_success_count=",class_id_success_count, " class_id_count=",class_id_count)
     class_accuracy_percent[class_id] = class_id_accuracy
     class_success_count[class_id] = class_id_success_count
