076f840d59866cbb978cb0a31efe78b53cdf4c89
pablo <pablo.bertorello@gmail.com>
2018-11-28 22:38:30 -0800

    refactored PA

--- svm-raw.py
+++ svm-raw.py
@@ -8,6 +8,12 @@ import skimage.transform
 import sys
 from sklearn.decomposition import PCA
 
+###
+# -1) 2 digit deecimal
+# 0) accuracy against train set: high/low -> overfit/bias
+# 1) x-validate: maximize use of train set
+# 2) param search in validation set: C, gamma
+# 3) xgradient: Sravan
 
 ## CONFIG
 max_taining_examples = 400
@@ -32,7 +38,7 @@ env_summary_path = local_summary_path
 # env_summary_path = sage_summary_path
 
 # HELPER
-def getResizedReshapedMatrix(image):
+def getRoundedResizedReshapedMatrix(image):
     target_size = 224
     number_colors = 3
     num_rows = image.shape[0]
@@ -42,18 +48,21 @@ def getResizedReshapedMatrix(image):
     # scaled = image.reshape(num_pixels, num_colors)
     resized = skimage.transform.resize(image, (target_size,target_size, number_colors))
     resizedReshaped = resized.reshape(1, target_size * target_size * number_colors)
-    return resizedReshaped
+    roundedResizedReshaped = np.around(resizedReshaped, decimals=2)
+    return roundedResizedReshaped
 
-def getZeroMeanNormalizedVarianceMatrix(Xmatrix):
+def getRoundedZeroMeanNormalizedVarianceMatrix(Xmatrix):
     # Mean subtraction: subtracting the mean across every individual feature in the data
     Xmatrix -= np.mean(Xmatrix, axis = 0)
-    print("XmatrixMean=",Xmatrix)
+    # print("XmatrixMean=",Xmatrix)
     # Normalization, two wasy:
     # 1) is to divide each dimension by its standard deviation, once it has been zero-centered
-    Xmatrix /= np.std(Xmatrix, axis = 0)
-    print("XmatrixVariance=", Xmatrix)
     # 2) Another form of this preprocessing normalizes each dimension so that the min and max along the dimension is -1 and 1 respectively
-    return Xmatrix
+    Xmatrix /= np.std(Xmatrix, axis = 0)
+    # print("XmatrixVariance=", Xmatrix)
+    roundedResizedReshaped = np.around(Xmatrix, decimals=2)
+    print("XmatrixNormalizedRounded=", roundedResizedReshaped)
+    return roundedResizedReshaped
 
 # RANDOM
 random.seed(229)
@@ -75,7 +84,7 @@ def getXY(setName):
             expected_quantity = xId_y[1]
             try:
                 this_image = imread(env_images_path+file_name)
-                image_resized_reshaped = getResizedReshapedMatrix(this_image)
+                image_resized_reshaped = getRoundedResizedReshapedMatrix(this_image)
                 image_to_use = image_resized_reshaped
                 if len(X_set)==0:
                     X_set = image_to_use
@@ -93,20 +102,28 @@ def getXY(setName):
 X_train,Y_train=getXY("counting_train")
 X_validation,Y_validation=getXY("counting_val")
 
-X_train_mean_variance_normalized = getZeroMeanNormalizedVarianceMatrix(X_train)
-X_validation_mean_variance_normalized = getZeroMeanNormalizedVarianceMatrix(X_validation)
+X_train_mean_variance_normalized = getRoundedZeroMeanNormalizedVarianceMatrix(X_train)
+X_validation_mean_variance_normalized = getRoundedZeroMeanNormalizedVarianceMatrix(X_validation)
 
 # PCA
+def getPcaMatrix(design_matrix, pca_model):
+    pca_matrix = pca_model.fit_transform(design_matrix)
+    roundedPcaMatrix = np.around(pca_matrix, decimals=2)
+    print("roundedPcaMatrix=", roundedPcaMatrix)
+    return roundedPcaMatrix
+
 # n_components=10000 must be between 0 and min(n_samples, n_features)=336 with svd_solver='full'
 pca = PCA(n_components= int(max_taining_examples/2))  # some examples may be missing (in the test set)
-x_pca = pca.fit_transform(X_train_mean_variance_normalized)
+X_train_pca_mean_variance_normalized = getPcaMatrix(X_train_mean_variance_normalized, pca)
+X_validation_pca_mean_variance_normalized = getPcaMatrix(X_validation_mean_variance_normalized, pca)
 
-X_train = pca.fit_transform(X_train_mean_variance_normalized)
-print("x_pca.shape=",x_pca.shape)
-X_validation_mean_variance_normalized = pca.fit_transform(X_validation_mean_variance_normalized)
-
-print ("X_train=", X_train_mean_variance_normalized)
 # exit(1)
+
+# FINAL MATRIX
+X_train_final = X_train_pca_mean_variance_normalized
+print (X_train_final.shape, " <= X_train_final=", X_train_final)
+X_validation_final = X_validation_pca_mean_variance_normalized
+print (X_validation_final.shape, " <= X_validation_final=", X_validation_final)
 
 
 # ACCURACY
@@ -138,7 +155,7 @@ def validate(param_string, trained_model, X_validation, Y_validation):
     class_count = np.zeros(classes_under_study)
 
     for class_id in range(classes_under_study):
-        class_id_accuracy, class_id_success_count, class_id_count = getAccuracy(param_string, trained_model, X_validation_mean_variance_normalized, Y_validation, class_id)
+        class_id_accuracy, class_id_success_count, class_id_count = getAccuracy(param_string, trained_model, X_validation, Y_validation, class_id)
         print(param_string, " class_id=", class_id, " class_id_accuracy=",class_id_accuracy, " class_id_success_count=",class_id_success_count, " class_id_count=",class_id_count)
         class_accuracy_percent[class_id] = class_id_accuracy
         class_success_count[class_id] = class_id_success_count
@@ -157,14 +174,14 @@ def validate(param_string, trained_model, X_validation, Y_validation):
 # Support Vector Classification for the case of a linear kernel. Note that LinearSVC does not accept keyword kernel,
 # as this is assumed to be linear.
 
-# KERNEL : string, optional (default=’rbf’)
+# KERNEL : string, optional (default=rbf)
 # Specifies the kernel type to be used in the algorithm.
-# It must be one of ‘linear’, ‘poly’, ‘rbf’, ‘sigmoid’, ‘precomputed’ or a callable. If none is given, ‘rbf’ will be used.
+# It must be one of linear, poly, rbf, sigmoid, precomputed or a callable. If none is given, rbf will be used.
 # If a callable is given it is used to precompute the kernel matrix.
-# GAMMA : float, optional (default=’auto’)
-# Kernel coefficient for ‘rbf’, ‘poly’ and ‘sigmoid’.
-# Current default is ‘auto’ which uses 1 / n_features, if gamma='scale' is passed then it uses 1 / (n_features * X.std()) as value of gamma.
-# The current default of gamma, ‘auto’, will change to ‘scale’ in version 0.22. ‘auto_deprecated’, a deprecated version of ‘auto’ is used as a default indicating that no explicit value of gamma was passed.
+# GAMMA : float, optional (default=auto)
+# Kernel coefficient for rbf, poly and sigmoid.
+# Current default is auto which uses 1 / n_features, if gamma='scale' is passed then it uses 1 / (n_features * X.std()) as value of gamma.
+# The current default of gamma, auto, will change to scale in version 0.22. auto_deprecated, a deprecated version of auto is used as a default indicating that no explicit value of gamma was passed.
 
 nu_range = [0.05, 0.10, 0.15]
 C_range = [1e-2, 1, 1e2]
@@ -180,17 +197,18 @@ for c_param in C_range:
         try:
             # clf = svm.NuSVC(nu=nu_param, )
             clf = svm.SVC(C=c_param, gamma=gamma_param)  # radial kernel
-            clf.fit(X_train_mean_variance_normalized, Y_train)
+            clf.fit(X_train_final, Y_train)
             # clf = svm.LinearSVC(loss='l2', penalty='l1', dual=False)
             # clf = svm.LinearSVC(penalty='l2')
             # clf = svm.NuSVC()
             # clf = svm.SVC(gamma='scale', decision_function_shape='ovo')
             # clf = svm.SVC(gamma='scale')
             # clf = svm.LinearSVC(penalty='l2', multi_class='ovr')
-            class_accuracy_percent = validate(param_string, clf, X_validation_mean_variance_normalized, Y_validation)
+            class_accuracy_percent = validate(param_string, clf, X_validation_final, Y_validation)
         except Exception:
             print("Unexpected error:", sys.exc_info())
             bad = True
+            class_accuracy_percent = 0
         param_validation_accuracy[param_string] = class_accuracy_percent
 
 print("param_validation_accuracy=", param_validation_accuracy)
